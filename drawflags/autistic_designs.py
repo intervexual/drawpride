dace3024aa481caad38be19168c27e4192361a96
intervexual <intersexdotsocial@proton.me>
2024-12-21 18:48:30 -0500

    Playing with some autism flag designs

--- drawflags/autistic_designs.py
+++ drawflags/autistic_designs.py
@@ -15,93 +15,99 @@ rainbow_stripes = ['#E40303', '#FF8C00', '#FFED00', '#008026', '#24408E', '#7329
 to_show = False
 outdir = 'output/intervex/'
 
-##
-# Making a flag inspired by the Autistic Pride Day (Australia) logo
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['white'])
-draw_concentric_infinities(d, ['#2cad74', 'white', '#ffc65b', 'white', '#e43546'], 'white', size_ratio=0.7)
-filelocations = save_flag(d, 'autistic_concentric', outdir, same_folder=True, show_image=to_show)
 
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, [disability_grey])
-draw_concentric_infinities(d, disability_stripes, disability_grey, size_ratio=0.7)
-filelocations = save_flag(d, 'autistic_codisabled', outdir, same_folder=True, show_image=to_show)
+############################### CONCENTRIC INFINITY DESIGNS
 
-# codisabled but specifically mad
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, [disability_grey])
-draw_concentric_infinities(d, ['#796098', '#bf7998', '#796098', '#bf7998'], disability_grey, size_ratio=0.7)
-filelocations = save_flag(d, 'mad_autistic', outdir,  same_folder=True, show_image=to_show)
-
-# rainbow on white
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#f7f7f7'])
-draw_concentric_infinities(d, rainbow_stripes, '#f7f7f7', size_ratio=0.7)
-filelocations = save_flag(d, 'autistic_queer', outdir, same_folder=True, show_image=to_show)
-
-# trans on white
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#f7f7f7'])
-draw_concentric_infinities(d, trans_stripes, '#f7f7f7', size_ratio=0.7)
-filelocations = save_flag(d, 'autistic_queer', outdir, same_folder=True, show_image=to_show)
-
-# autism as disability
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#f7f7f7'])
-draw_concentric_infinities(d, [disability_grey] + disability_stripes + [disability_grey], '#f7f7f7', size_ratio=0.7)
-filelocations = save_flag(d, 'autistic_as_disability', outdir, same_folder=True, show_image=to_show)
-
-# ROYLG on grey
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, [disability_grey])
-draw_concentric_infinities(d, ['#2cad74', '#98d570', '#ffc65b',  '#f68249', '#e43546'], disability_grey, size_ratio=0.7)
-filelocations = save_flag(d, 'autistic_hybrid', outdir, same_folder=True, show_image=to_show)
-
-#draw_concentric_infinities(d, [disability_grey, '#243444',  '#3dcec1', '#eae186', 'white',  '#eae186', '#3dcec1', '#243444'], size_ratio=0.7)
-#filelocations = save_flag(d, 'autistic_teal', outdir, same_folder=True, show_image=to_show)
-
-# ND on ROYLG diagonal
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#f7f7f7'])
-draw_diagonal_stripes(d, ['#2cad74', '#98d570'] + ['#ffc65b']*6 + ['#f68249', '#e43546'])
-embed_icon(d, 'disability/neurodiversity.svg', {})
-filelocations = save_flag(d, 'autistic_ryg_rainb_diag', outdir, same_folder=True, show_image=to_show)
-
-# riffing on https://www.tumblr.com/themogaidragon/762687565710213120/hi-i-just-wanted-to-ask-if-you-would-accept-an?source=share
-# and https://www.tumblr.com/themogaidragon/701717045607317504/pathological-demand-avoidance?source=share
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#2cad74', '#98d570', '#ffc65b', 'white', '#ffc65b', '#f68249', '#e43546'])
-draw_circle(d, 'white', size_ratio=0.6)
-embed_icon(d, 'disability/aufinity2.png', {}, size_ratio=0.5)
-filelocations = save_flag(d, 'autistic_ryg_belt_aufinity', outdir, same_folder=True, show_image=to_show)
+# ideas for stripes
+#apd_colours = ['#2cad74', 'white', '#ffc65b', 'white', '#e43546']
+roylg_colours = ['#2cad74', '#98d570', '#ffc65b',  '#f68249', '#e43546']
+mad_colours = ['#796098', '#bf7998', '#796098', '#bf7998']
+disabled_colours = [disability_grey] + disability_stripes + [disability_grey]
+stripe_options = {'roylg':roylg_colours, 'mad':mad_colours,
+                  'disabled':disabled_colours, 'lgbt':rainbow_stripes, 'trans':trans_stripes} # 'apd':apd_colours,
 
 
-# riffing on https://www.tumblr.com/themogaidragon/762687565710213120/hi-i-just-wanted-to-ask-if-you-would-accept-an?source=share
-# and https://www.tumblr.com/themogaidragon/701717045607317504/pathological-demand-avoidance?source=share
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#2cad74', '#98d570', '#ffc65b', 'white', '#ffc65b', '#f68249', '#e43546'])
-draw_circle(d, 'white', size_ratio=0.6)
-embed_icon(d, 'disability/aufinity2.png', {}, size_ratio=0.5)
-filelocations = save_flag(d, 'autistic_ryg_belt_aufinity', outdir, same_folder=True, show_image=to_show)
+# ideas for backgrounds
+bg_options = {'white':'white', 'whitish':'#f7f7f7', 'disgrey':disability_grey}
 
-# simple aufinity flag
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#fbfbfb'])
-embed_icon(d, 'disability/aufinity2.png', {}, size_ratio=1)
-filelocations = save_flag(d, 'autistic_aufinity', outdir, same_folder=True, show_image=to_show)
+for bg in bg_options:
+    for stripe_scheme in stripe_options:
+        d = draw.Drawing(500 * 2, 300 * 2)
+        draw_horiz_bars(d, [ bg_options[bg] ])
+        draw_concentric_infinities(d, stripe_options[stripe_scheme], bg_options[bg], size_ratio=0.7)
+        filename = f'autistic_concentric_{stripe_scheme}_on_{bg}'
+        filelocations = save_flag(d, filename, outdir, same_folder=True, show_image=to_show)
 
-# simple gold loop thanks to ryanyflags
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#fbfbfb'])
-embed_icon(d, 'disability/gold_infinity.svg', {}, size_ratio=1.2)
-filelocations = save_flag(d, 'autistic_gold', outdir, same_folder=True, show_image=to_show)
 
-# simple autism spectrum nautilus flag
-d = draw.Drawing(500*2, 300*2)
-draw_horiz_bars(d, ['#f7f7f7'])
+
+############################### ICON ON A WHITE(ISH) BACKGROUND DESIGNS
+
 spectrum_rainbow = ['#cf7280', '#dfa87c', '#eede77', '#95c77a', '#3bb07d', '#5bb9af', '#7bc2e0', '#97a7c0', '#b38da0']
-draw_nautilus(d, spectrum_rainbow, orientation=HORIZONTAL, size_ratio=1.25, stretch_ratio=2.25)
-filelocations = save_flag(d, 'autistic_nautilus', outdir, same_folder=True, show_image=to_show)
 
+icon_options = {'aufinity':'disability/aufinity_rainbow.png',
+                'gold_infinity':'disability/infinity_gold_ryanyflags.svg',
+                'rainbow_infinity':'disability/neurodiversity_rainbow.svg',
+                'rainbow_ADHD':'disability/ADHD_butterfly_rainbow.svg',
+                'nautilus':draw_nautilus
+                }
+sizes = {'aufinity':0.99,
+         'gold_infinity':1.2,
+         'rainbow_infinity':1.2,
+         'rainbow_ADHD':1.1,
+         'nautilus':1.25}
+
+for bg in bg_options:
+    for icon_name in icon_options:
+        d = draw.Drawing(500 * 2, 300 * 2)
+        draw_horiz_bars(d, [ bg_options[bg] ])
+        if type(icon_options[icon_name]) == str:
+            embed_icon(d, icon_options[icon_name], {}, size_ratio=sizes[icon_name])
+        else:
+            icon_options[icon_name](d, spectrum_rainbow, stretch_ratio=2.25, size_ratio=sizes[icon_name])
+        filename = f'plain_{icon_name}_on_{bg}'
+        filelocations = save_flag(d, filename, outdir, same_folder=True, show_image=to_show)
+
+
+############################### ICON ON A DIAGONAL ROYLG BACKGROUND
+
+for icon_name in icon_options:
+    if 'ADHD' not in icon_name:
+        d = draw.Drawing(500 * 2, 300 * 2)
+        draw_diagonal_stripes(d, ['#2cad74', '#98d570'] + ['#ffc65b']*6 + ['#f68249', '#e43546'])
+        scaling = 0.9
+        if 'infinity' in icon_name:
+            scaling = 0.8
+        if type(icon_options[icon_name]) == str:
+            embed_icon(d, icon_options[icon_name], {}, size_ratio=sizes[icon_name]*scaling)
+        else:
+            icon_options[icon_name](d, spectrum_rainbow, stretch_ratio=2.25, size_ratio=sizes[icon_name]*scaling)
+        filename = f'diagonal_{icon_name}_on_roylg'
+        filelocations = save_flag(d, filename, outdir, same_folder=True, show_image=to_show)
+
+
+############################## ICON IN A BELT ON HORIZONTAL STRIPES
+
+# riffing on https://www.tumblr.com/themogaidragon/762687565710213120/hi-i-just-wanted-to-ask-if-you-would-accept-an?source=share
+# and https://www.tumblr.com/themogaidragon/701717045607317504/pathological-demand-avoidance?source=share
+
+horiz_stripe_options = {'teal':['#243444',  '#3dcec1', '#eae186', 'white',  '#eae186', '#3dcec1', '#243444'],
+                        'roylg':['#2cad74', '#98d570', '#ffc65b', 'white', '#ffc65b', '#f68249', '#e43546']}
+
+for bg in horiz_stripe_options:
+    for icon_name in icon_options:
+        if 'ADHD' not in icon_name:
+            d = draw.Drawing(500 * 2, 300 * 2)
+            draw_horiz_bars(d, horiz_stripe_options[bg])
+            draw_circle(d, 'white', size_ratio=0.6)
+            scaling = 0.4
+            if icon_name == 'aufinity':
+                scaling = 0.5
+
+            if type(icon_options[icon_name]) == str:
+                embed_icon(d, icon_options[icon_name], {}, size_ratio=sizes[icon_name]*scaling)
+            else:
+                icon_options[icon_name](d, spectrum_rainbow, stretch_ratio=2.25, size_ratio=sizes[icon_name]*scaling)
+            filename = f'horizontal_{icon_name}_on_{bg}'
+            filelocations = save_flag(d, filename, outdir, same_folder=True, show_image=to_show)
 
 
